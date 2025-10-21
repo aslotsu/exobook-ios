626fe0eb7a7d2fbc6057d05d7f58a60d59f71cc5
Alfred Lotsu <alfredlotsu2@gmail.com>
2025-10-21 20:15:36 +0000

    Initial Commit

---
+++ Exobook/ExobookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExobookApp.swift
+//  Exobook
+//
+//  Created by Alfred Lotsu on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExobookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
